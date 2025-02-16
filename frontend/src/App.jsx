ebbf7345a598288425b4b122229e5244284b06ea
Jinith Joseph <jinithj@gmail.com>
2025-02-16 19:56:10 +0400

    Working Backend and UI before Chakra

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,16 @@
-import { Container, Stack, Text } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import UserGrid from "./components/UserGrid";
-import { useState } from "react";
-
-// updated this after recording. Make sure you do the same so that it can work in production
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-	const [users, setUsers] = useState([]);
+  const [count, setCount] = useState(0)
 
-	return (
-		<Stack minH={"100vh"}>
-			<Navbar setUsers={setUsers} />
-
-			<Container maxW={"1200px"} my={4}>
-				<Text
-					fontSize={{ base: "3xl", md: "50" }}
-					fontWeight={"bold"}
-					letterSpacing={"2px"}
-					textTransform={"uppercase"}
-					textAlign={"center"}
-					mb={8}
-				>
-					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
-						My Besties
-					</Text>
-					🚀
-				</Text>
-
-				<UserGrid users={users} setUsers={setUsers} />
-			</Container>
-		</Stack>
-	);
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
